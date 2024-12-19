072c1ffe6aa0fd72adb658e42e0b0a672cb9e9cc
Matheus Barbosa <122236890+mthszr@users.noreply.github.com>
2024-12-19 16:40:46 -0300

    tools animations

--- code/player.py
+++ code/player.py
@@ -1,6 +1,7 @@
 import pygame
 from settings import *
 from support import *
+from timer import Timer
 
 class Player(pygame.sprite.Sprite): 
     def __init__(self, position, group):  
@@ -19,10 +20,22 @@ class Player(pygame.sprite.Sprite):
         self.position = pygame.math.Vector2(self.rect.center) # The player's position
         self.speed = 200
 
+        # Timer
+        self.timers = {
+            'tool use': Timer(350,self.use_tool)
+        }
+        
+        
+        # Tools
+        self.selected_tool = 'axe'
+
+    def use_tool(self):
+        print(self.selected_tool)
+    
     def import_assets(self):
         # Import the player's animations
         self.animations = { 'up': [], 'down': [], 'left': [], 'right': [],
-                           'righ_idle': [], 'left_idle': [], 'up_idle': [], 'down_idle': [],
+                           'right_idle': [], 'left_idle': [], 'up_idle': [], 'down_idle': [],
                            'right_hoe': [], 'left_hoe': [], 'up_hoe': [], 'down_hoe': [],
                            'right_axe': [], 'left_axe': [], 'up_axe': [], 'down_axe': [],
                            'right_water': [], 'left_water': [], 'up_water': [], 'down_water': [] }
@@ -43,30 +56,45 @@ class Player(pygame.sprite.Sprite):
     def input(self): 
         keys = pygame.key.get_pressed() # Get the keys that are pressed
 
-        if keys[pygame.K_UP]:
-            self.direction.y = -1
-            self.status = 'up'
-        elif keys[pygame.K_DOWN]:
-            self.direction.y = 1
-            self.status = 'down'
-        else:
-            self.direction.y = 0 # If no keys are pressed, set the y direction to 0
-        
+        if not self.timers['tool use'].active: # If the player is not using a tool
+            # Change the player's direction
+            if keys[pygame.K_UP]:
+                self.direction.y = -1
+                self.status = 'up'
+            elif keys[pygame.K_DOWN]:
+                self.direction.y = 1
+                self.status = 'down'
+            else:
+                self.direction.y = 0 # If no keys are pressed, set the y direction to 0
+            
 
-        if keys[pygame.K_LEFT]:
-            self.direction.x = -1
-            self.status = 'left'
-        elif keys[pygame.K_RIGHT]:
-            self.direction.x = 1
-            self.status = 'right'
-        else:
-            self.direction.x = 0 # If no keys are pressed, set the x direction to 0
+            if keys[pygame.K_LEFT]:
+                self.direction.x = -1
+                self.status = 'left'
+            elif keys[pygame.K_RIGHT]:
+                self.direction.x = 1
+                self.status = 'right'
+            else:
+                self.direction.x = 0 # If no keys are pressed, set the x direction to 0
 
+            # Tool use
+            if keys[pygame.K_SPACE]:
+                self.timers['tool use'].activate()
+                self.direction = pygame.math.Vector2() # Set the direction to 0
+                self.frame_index = 0 # Guarantee the animation starts from the beginning
+
+    def update_timers(self):
+        for timer in self.timers.values():
+            timer.update()
+    
     def get_status(self):
-        # Idle animation
-        if self.direction.magnitude() == 0:
-            self.status = self.status.split('_')[0] + '_idle'
+            # Idle animation
+            if self.direction.magnitude() == 0:
+                self.status = self.status.split('_')[0] + '_idle'
 
+            # Tool animations
+            if self.timers['tool use'].active:
+                self.status = self.status.split('_')[0] + '_' + self.selected_tool
 
     # Move the player
     def move(self, dt):
@@ -85,6 +113,8 @@ class Player(pygame.sprite.Sprite):
     def update(self, dt):
         self.input()
         self.get_status()
+        self.update_timers()
+
         self.move(dt)
         self.animate(dt)
 
