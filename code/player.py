3fbfcbfbf230fe05f543a374d452c8e562d5e2c6
Matheus Barbosa <122236890+mthszr@users.noreply.github.com>
2024-12-19 17:38:06 -0300

    added switching tools

--- code/player.py
+++ code/player.py
@@ -22,15 +22,27 @@ class Player(pygame.sprite.Sprite):
 
         # Timer
         self.timers = {
-            'tool use': Timer(350,self.use_tool)
-        }
-        
+            'tool use': Timer(350,self.use_tool),
+            'tool switch': Timer(200),
+            'seed use': Timer(350,self.use_seed),
+            'seed switch': Timer(200)
+        }     
         
         # Tools
-        self.selected_tool = 'axe'
+        self.tools = ['hoe', 'axe', 'water']
+        self.tool_index = 0
+        self.selected_tool = self.tools[self.tool_index]
+
+        # Seeds
+        self.seeds = ['corn', 'tomato']
+        self.seed_index = 0
+        self.selected_seed = self.seeds[self.seed_index]
 
     def use_tool(self):
-        print(self.selected_tool)
+        pass
+    
+    def use_seed(self):
+        pass
     
     def import_assets(self):
         # Import the player's animations
@@ -82,6 +94,25 @@ class Player(pygame.sprite.Sprite):
                 self.timers['tool use'].activate()
                 self.direction = pygame.math.Vector2() # Set the direction to 0
                 self.frame_index = 0 # Guarantee the animation starts from the beginning
+
+            # Change the selected tool
+            if keys[pygame.K_q] and not self.timers['tool switch'].active:
+                self.timers['tool switch'].activate()
+                self.tool_index += 1
+                self.selected_tool = self.tools[self.tool_index % len(self.tools)]
+
+            # Seed use
+            if keys[pygame.K_LCTRL]:
+                self.timers['seed use'].activate()
+                self.direction = pygame.math.Vector2() # Set the direction to 0
+                self.frame_index = 0 # Guarantee the animation starts from the beginning
+
+            # Change the selected seed
+            if keys[pygame.K_e] and not self.timers['seed switch'].active:
+                self.timers['seed switch'].activate()
+                self.seed_index += 1
+                self.selected_seed = self.seeds[self.seed_index % len(self.seeds)]
+
 
     def update_timers(self):
         for timer in self.timers.values():
