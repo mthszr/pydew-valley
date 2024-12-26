5d89391eaa06727c48e1f2fc27aaa7ccadb4f30e
Matheus Barbosa <122236890+mthszr@users.noreply.github.com>
2024-12-26 08:44:50 -0300

    daytime transition

--- code/level.py
+++ code/level.py
@@ -7,7 +7,7 @@ from pytmx.util_pygame import load_pygame
 from support import *
 from transition import Transition
 from soil import SoilLayer
-from sky import Rain
+from sky import Rain, Sky
 from random import randint
 
 class Level:
@@ -30,6 +30,7 @@ class Level:
         self.rain = Rain(self.all_sprites)
         self.raining = randint(0, 10) > 3
         self.soil_layer.raining = self.raining
+        self.sky = Sky()
 
     def setup(self):
         tmx_data = load_pygame('./data/map.tmx')
@@ -110,6 +111,9 @@ class Level:
             for apple in tree.apple_sprites.sprites():
                 apple.kill()
             tree.create_fruits()
+
+        # Sky
+        self.sky.start_color = [255, 255, 255]
     
     def plant_collision(self):
         if self.soil_layer.plant_sprites:
@@ -131,6 +135,9 @@ class Level:
         # Rain
         if self.raining:
             self.rain.update()
+
+        # Day and night cycle
+        self.sky.display(dt)
         
         # Transition overlay
         if self.player.sleep:
